6b2a1406caa4a0e010d2a7cfc8a562511dfa1303
EliottLacommeRuiz <EliottLacommeRuiz2027@u.northwestern.edu>
2024-01-14 23:44:03 -0600

    Create SensorMapping.ino
    Basic layout of the sensor tests. Instructions are in the script and places to put their values are also there.
    
    Still need to fill out the main functions. For some we can use some basic wiring (like the servo and a pot) to check their range.

---
+++ sensor_test_scripts/SensorTestCenter/SensorMapping/SensorMapping.ino
@@ -0,0 +1,74 @@
+//Include any libraries here:
+#include <Servo.h>
+
+
+//This script will be used to get the values from all of the sensors that we need so we know their optimal ranges for mapping. 
+//  Wire each of them idividually and take measurements of their maximum and minimum values. For the servo use at least 2 to check for manufacturing error range.
+
+// Here is a list of all of the sensors' ranges we should get familiar with: 
+//    - Servo
+//        - MAX:
+//        - NEUTRAL:
+//        - MIN:
+//    - Absolute Orientation Sensor
+//        - MAX:
+//        - NEUTRAL:
+//        - MIN:
+//    - Accelerometer
+//        - MAX:
+//        - NEUTRAL:
+//        - MIN:
+//    - Current Sensor
+//        - MAX:
+//        - NEUTRAL:
+//        - MIN:
+//    - GPS
+//        - MAX:
+//        - NEUTRAL:
+//        - MIN:
+//    - Altimiter
+//        - MAX:
+//        - NEUTRAL:
+//        - MIN:
+
+
+void setup() {
+  //Setup pinmodes 
+
+}
+
+void loop() {
+  //gpsRead();
+  //altRead();
+  //accelRead();
+  //currRead();
+  //servoRead();
+  //AOSRead();
+}
+
+void gpsRead() {
+
+}
+
+void altRead() {
+
+}
+
+void accelRead() {
+
+}
+
+void currRead() {
+
+}
+
+void servoRead() {
+
+}
+
+void AOSRead() {
+
+}
+
+
+
