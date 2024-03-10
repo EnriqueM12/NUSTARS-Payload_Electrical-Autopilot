b44ff2761e2ba8e3479bd0fe60d0f86c17ea35b2
EliottLacommeRuiz <EliottLacommeRuiz2027@u.northwestern.edu>
2024-03-10 10:25:13 -0500

    Update EjectionCodeForLaunch.ino

--- ejection/EjectionCodeForLaunch/EjectionCodeForLaunch.ino
+++ ejection/EjectionCodeForLaunch/EjectionCodeForLaunch.ino
@@ -2,12 +2,18 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP3XX.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
 #include <SD.h>
 #include <TimeLib.h>
 
 #define SEALEVELPRESSURE_HPA (1013.25)
+#define BNO055_SAMPLERATE_DELAY_MS (100) 
 
 Adafruit_BMP3XX altimeter; 
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28, &Wire); // WIRE <--- VERY IMPORTANT!!!!!! 
+
+
 float feet;
 float altInit;
 
@@ -39,6 +45,7 @@ void setup() {
   Serial.begin (115200);
 
   altimeter.begin_I2C();
+  bno.begin();
 
   // altimeter setup
   altimeter.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
@@ -53,14 +60,13 @@ void setup() {
   ejectServo.attach(8);
   
   ejectMin = 0;
-  ejectMax = 180;
+  ejectMax = 90;
 
   ejectServo.write(ejectMin);
 
   altInit = 0;
   for (int i = 0; i < 50; i++) {
     altInit += altimeter.readAltitude(SEALEVELPRESSURE_HPA) * 3.2808399;
-    //Serial.print(altInit); Serial.print(" ");
     delay(100);
   }
   altInit = altInit / 50;
@@ -86,21 +92,15 @@ void loop() {
 
   logData();
   servoMove();
-
-  Serial.print("Feet: "); Serial.print(feet);
-  Serial.print("\t\tChannel: "); Serial.println(chEject);
-  
-
-  delay(50);
 }
 
 void servoMove() {
   if (feet <= 400) {
     if (chEject >= 1870) {
-      ejectServo.write(ejectMax);
+      ejectServo.write(ejectMin);
     }
     else if (chEject <= 1870) {
-      ejectServo.write(ejectMin);
+      ejectServo.write(ejectMax);
     }
       
   }
@@ -108,28 +108,47 @@ void servoMove() {
 
 void logData() { // Prints the altimeter and 
   // Time 
-  File dataFile;
-  currentMinute = minute();
-  currentSecond = second();
-  currentMillis = millis() % 1000;
-  
+  File dataFile;  
   dataFile = SD.open("data.txt", FILE_WRITE);
+
+  sensors_event_t eventAOS;
+  bno.getEvent(&eventAOS);
+
+  float xOrient = eventAOS.orientation.x;
+  float yOrient = eventAOS.orientation.y;
+  float zOrient = eventAOS.orientation.z;
+
+  float xAccel = eventAOS.acceleration.x;
+  float yAccel = eventAOS.acceleration.y;
+  float zAccel = eventAOS.acceleration.z;
 
   String dataString = "";
 
 
   // Logging
-  dataString += "Altitude: " + String(feet) + "\t\t\t";
-  if (feet <= 400) {
-    dataString += "*********BELOW 400 BELOW 400 BELOW 400**********\t\t\t";
-  }
-  if (chEject >= 1870) {
-    dataString += "*********ARMED ARMED ARMED**********\t\t\t";
-  }
-  dataString += String(currentMinute) + ":" + String(currentSecond) + ":" + String(currentMillis) + "\n";
 
+  //if (millis() % 100 == 0) {
+    dataString += String(feet) + ", " 
+                + String(xAccel) + ", " + String(yAccel) + ", " + String(zAccel) + ", " 
+                + String(xOrient) + ", " + String(yOrient) + ", " + String(zOrient);
+
+    if (feet <= 400) {
+      dataString += ", below, ";
+    }
+    else {
+      dataString += ", above, ";
+    }
+    if (chEject >= 1870) {
+      dataString += ", armed, \n";
+    }
+    else {
+      dataString += ", unarmed, \n";
+    }
+  //}
   dataFile.println(dataString);
   dataFile.close();
+
+  delay(BNO055_SAMPLERATE_DELAY_MS);
 }
 
 
